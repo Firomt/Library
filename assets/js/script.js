1ec091a10996df63ca8ec895a98481e13333968e
Firomt <firomsamt@gmail.com>
2025-02-18 10:18:27 +0300

    add book constructor and function to add new book to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,23 @@ bookDialog.addEventListener("click", (event) => {
         bookDialog.close();
     }
 });
+
+
+    const myLibrary = []; // Array to store book objects
+
+    // Book constructor function
+    function Book(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
+
+    // Function to add a new book to the library array
+    function addBookToLibrary(title, author, pages, readStatus) {
+        const newBook = new Book(title, author, pages, readStatus);
+        myLibrary.push(newBook);
+        displayBooks(); // Refresh display after adding new book
+    }
+
+
